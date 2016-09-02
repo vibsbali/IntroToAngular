bb2af83c83180ccb4dd9cd30e7afd1f8f9ab053d
Vaibhav Bali <vibsbali@gmail.com>
2016-09-02 14:49:24 +1000

    Created an Employees Controller

---
+++ AngularApp/ApiControllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace AngularApp.ApiControllers
+{
+    public class EmployeeController : ApiController
+    {
+        private readonly List<Employee> employees = new List<Employee>
+        {
+            new Employee { Id = 1003, Name = "Kevin Wong", Gender = "Male", Salary = 4000.00m, DepartmentId = 1003  },
+            new Employee { Id = 1004, Name = "Samantha Hogan", Gender = "Female", Salary = 3000.00m, DepartmentId = 1004  },
+            new Employee { Id = 1005, Name = "Sean Campbell", Gender = "Male", Salary = 2000.00m, DepartmentId = 1005  },
+            new Employee { Id = 1006, Name = "Nooka Aurera", Gender = "Male", Salary = 1000.00m, DepartmentId = 1022  },
+            new Employee { Id = 1007, Name = "Nikki Guerara", Gender = "Female", Salary = 5000.00m, DepartmentId = 3071  }
+
+        };
+
+        [HttpGet]
+        public IEnumerable<Employee> GetDepartments()
+        {
+            return employees;
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public decimal Salary { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
