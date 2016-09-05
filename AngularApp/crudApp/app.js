93dec5477e0e924090012522d40cd62c437d5d4e
Vaibhav Bali <vibsbali@gmail.com>
2016-09-05 21:39:15 +1000

    Created an Http service to fetch data

---
+++ AngularApp/crudApp/app.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+
+(function() {
+    var app = angular.module("myApp", []);
+
+    app.factory("CrudEmployeeService", function($http) {
+        var crudEmployee = {};
+
+        crudEmployee.getEmployees = function() {
+            var result = $http({
+                method: 'GET',
+                url: '/api/employee'
+            }).then(function(response) {
+                return response.data;
+            });
+            return result;
+        };
+
+        return crudEmployee;
+    });
+
+    app.controller("employeesController", function (CrudEmployeeService) {
+        var vm = this;
+
+        CrudEmployeeService.getEmployees().then(function (result) {
+            vm.employees = result;
+        },function(error) {
+            vm.error = error;
+        });
+    });
+}())
