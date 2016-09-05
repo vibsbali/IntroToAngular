30b97a290d09a48cb4b7b9c84bd493bd67d9b2ff
Vaibhav Bali <vibsbali@gmail.com>
2016-09-05 20:58:32 +1000

    Completed service and factory

---
+++ AngularApp/appCalculator/app.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+(function() {
+    var app = angular.module("myCalc", []);
+
+    app.service("myCalcService", function() {
+        this.add = function(a, b) {
+            return a + b;
+        };
+
+        this.substract = function(a, b) {
+            return a - b;
+        };
+    });
+
+    //myCalcService passed to the controller is called dependency injection
+    app.controller("myCalcController", function(myCalcService) {
+        var vm = this;
+
+        vm.add = function (a, b) {
+            vm.result = myCalcService.add(parseInt(a), parseInt(b));
+        };
+
+        vm.substract = function(a, b) {
+            vm.result = myCalcService.substract(a, b);
+        };
+
+    });
+
+}())
---
+++ AngularApp/appCalculator/appFactory.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+(function() {
+    var app = angular.module("myCalc", []);
+
+    app.factory("myCalcService", function () {
+        var factory = {};
+        factory.add = function (a, b) {
+            return a + b;
+        };
+
+        factory.substract = function (a, b) {
+            return a - b;
+        };
+
+        return factory;
+    });
+
+    //myCalcService passed to the controller is called dependency injection
+    app.controller("myCalcController", function (myCalcService) {
+        var vm = this;
+
+        vm.add = function (a, b) {
+            vm.result = myCalcService.add(parseInt(a), parseInt(b));
+        };
+
+        vm.substract = function (a, b) {
+            vm.result = myCalcService.substract(a, b);
+        };
+
+    });
+}());
